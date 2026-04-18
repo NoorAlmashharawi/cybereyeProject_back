42482eab8190c1502c1920913c68bdeddb8ad0d5
NoorAlmashharawi <noormalmashhrawi@gmail.com>
2026-04-18 12:25:57 +0300

    دمج شغل بسمة بالكويزات وحل التعارضات وتعارض ملف الكمبوزر عشان تحديث php

--- app/Models/Instructor.php
+++ app/Models/Instructor.php
@@ -4,16 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\SoftDeletes;
-
-=======
 use App\Models\User1;
 
-use Illuminate\Database\Eloquent\softDeletes;
 
 
->>>>>>> fb9c55c067aa10cfad48f088973bb8efdfefd44f
 class Instructor extends Model
 {
     use HasFactory, SoftDeletes;
@@ -26,22 +21,9 @@ class Instructor extends Model
         'enrollment_date'
     ];
 
-<<<<<<< HEAD
-    protected $casts = [
-=======
-   protected $fillable = [
-
-            'specialization',
-            'experience_years',
-            'rating',
-            'bio',
-            'enrollment_date'
-            ];
-
 
 
   protected $casts = [
->>>>>>> fb9c55c067aa10cfad48f088973bb8efdfefd44f
         'enrollment_date' => 'date',
     ];
 
